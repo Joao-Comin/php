f78d3f2f3717a1ddea283ac5d3d1ac7d600a21ae
Joao-Comin <joaovictorcomin2005@gmail.com>
2024-10-23 17:57:14 -0300

    Começando a aprender e usar constantes

--- projeto01/index.php
+++ projeto01/index.php
@@ -4,21 +4,6 @@ require_once 'sistema/configuracao.php';
 include_once 'funcoes.php';
 
 
-// if(ValidarEmail('teste@gmaill.com')){
-//     echo'Endereço de email válido';
-// }else{  
-//     echo 'Email Incorreto';
-// }
-
-
-// var_dump(validarUrl('http://teste.com'));
-// if (validarUrl('http://teste.com')) {
-//     echo 'URL válida';
-// } else {
-//     echo 'URL invalida';
-// }
-
-$url = 'http://unset.com';
-var_dump(validarUrl($url));
-echo'<hr>';
-var_dump(validarUrlComFiltro($url)); 
+echo SITE_NOME;
+echo '<hr>';
+echo constant('SITE_NOME');
--- projeto01/sistema/configuracao.php
+++ projeto01/sistema/configuracao.php
@@ -1,3 +1,13 @@
 <?php
 //echo"Arquivo de Configuração do Sistema";
+//define o horario
 date_default_timezone_set('America/Sao_Paulo');
+
+//informações do site
+define('SITE_NOME','Unset');
+define('SITE_DESCRIÇÃO','Unset - Tecnologia em Sistemas');
+
+//urls do sistema
+define('URL_PRODUCAO','http://unset.com.br');
+define('URL_DESENVOLVIMENTO','http://localhost/blog');
+
