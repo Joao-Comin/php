1f5ccad6ba5328c6d9b6c972fce385cdb520b93f
Joao-Comin <joaovictorcomin2005@gmail.com>
2024-10-24 16:56:40 -0300

    Criado uma nova Pasta de De nome Núcleo e uma Class de Nome Mensagem

--- projeto01/index.php
+++ projeto01/index.php
@@ -4,8 +4,12 @@
 
 require_once 'sistema/configuracao.php';
 include_once 'funcoes.php';
+include './sistema/Nucleo/Mensagem.php';
 
-$cpf = '111.433.699-80';
-var_dump(validarCpf($cpf));
+$msg = new Mensagem();
+var_dump($msg);
+
+// $cpf = '111.433.699-80';
+// var_dump(validarCpf($cpf));
 
 // echo $limparNumero = preg_replace('/[^0-9]/','', $cpf);
