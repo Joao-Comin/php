61ef9c5f40b68a9ddac2dd8e1c26cdaf9732f0fc
Joao-Comin <joaovictorcomin2005@gmail.com>
2024-10-24 17:49:41 -0300

    Adicionei bootstrap para ter mensagem de com cores e funções para cada mensagem

--- projeto01/index.php
+++ projeto01/index.php
@@ -1,3 +1,4 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <?php
 
 //localhost link: http://localhost:8081/projeto01/index.php
@@ -6,12 +7,11 @@ require_once 'sistema/configuracao.php';
 include_once 'funcoes.php';
 include './sistema/Nucleo/Mensagem.php';
 
+
+
 $msg = new Mensagem();
-echo $msg->renderizar();
+echo $msg->sucesso('Mensagem de Sucesso')->renderizar();
 echo '<hr>';
+echo $msg->erro('Mensagem de Erro')->renderizar();
+echo'<hr>';
 var_dump($msg);
-
-// $cpf = '111.433.699-80';
-// var_dump(validarCpf($cpf));
-
-// echo $limparNumero = preg_replace('/[^0-9]/','', $cpf);
--- projeto01/sistema/Nucleo/Mensagem.php
+++ projeto01/sistema/Nucleo/Mensagem.php
@@ -6,8 +6,21 @@ class Mensagem
     private $css;
 
 
+    public function sucesso(string $mensagem): Mensagem
+    {
+        $this->css = 'alert alert-success';
+        $this->texto = $this->filtrar($mensagem);
+        return $this;
+    }
+
+    public function erro(string $mensagem): Mensagem
+    {
+        $this->css = 'alert alert-danger';
+        $this-> texto = $this->filtrar($mensagem);
+        return $this;
+    }
     public function renderizar() : string{
-        return $this->texto = $this->filtrar("<h1>mensagem de teste");
+        return "<div class='{$this->css}'>{$this->texto}</div>";
     }
 
     private function filtrar(string $mensagem) : string{
