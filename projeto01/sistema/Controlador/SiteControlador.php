cd261ed7a2c31ee26dec74fee88af6f470ec35f3
Joao-Comin <joaovictorcomin2005@gmail.com>
2024-10-29 13:48:39 -0300

    Trabalhando com If e Constantes no Twig Template

--- projeto01/sistema/Controlador/SiteControlador.php
+++ projeto01/sistema/Controlador/SiteControlador.php
@@ -11,15 +11,15 @@ class SiteControlador extends Controlador {
     public function index(): void
     {
         echo $this->template->renderizar('index.html',[
-            'titulo' => 'teste de titulo',
-            'subtitulo' => 'teste de subtitulo'
+            
+            
         ]);
     }
     public function sobre():void
     {
         echo $this->template->renderizar('sobre.html',[
             'sobre' => 'sobre o site',
-            'teste' => 'teste de subtitulo'
+            'titulo' => 'Sobre Nós'
         ]);
     }
 }
