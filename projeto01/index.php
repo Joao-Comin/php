42b5f892888fd5b25b34ecea7829042661574119
Joao-Comin <joaovictorcomin2005@gmail.com>
2024-11-04 15:45:05 -0300

    Começando a aprender Sessão

--- projeto01/index.php
+++ projeto01/index.php
@@ -3,14 +3,18 @@
 //localhost link: http://localhost:8081/projeto01/index.php
 require __DIR__ . '/vendor/autoload.php';
 
-require 'Rotas.php';
+//require 'Rotas.php';
 
-// $dados = filter_input_array(INPUT_GET,FILTER_DEFAULT);
-// if(isset($dados)){
-//     echo $dados['nome'].'<hr>';
-//     echo $dados['senha'].'<hr>';
-// }
+$sessao = new Sistema\Nucleo\Sessao();
 
+// $sessao->criar('nome','joao' );
 
-// echo '<hr>';
+var_dump($sessao->carregar());
+echo '<hr>';
+var_dump($sessao->checar('nome'));
+echo'<hr>';
+//$sessao->limpar('nome');
+// var_dump($sessao->checar('usuario'));
+echo'<hr>';
+$sessao->deletar();
 // ?>
